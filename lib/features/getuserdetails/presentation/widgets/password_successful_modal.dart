05a8d961645d966a323064a7783faafdf962097d
ibidapo <ibidapodavid10@gmail.com>
2023-09-05 10:35:54 +0100

    change password modal added

---
+++ lib/features/getuserdetails/presentation/widgets/password_successful_modal.dart
@@ -0,0 +1,79 @@
+import 'package:afroreads/app/styles/fonts.dart';
+import 'package:afroreads/app/view/widget/busy_button.dart';
+import 'package:afroreads/core/constants/app_colors.dart';
+import 'package:afroreads/core/navigators/route_name.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:gap/gap.dart';
+
+
+class PasswordSuccesfluModal extends StatelessWidget {
+  const PasswordSuccesfluModal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+  
+    return Container(
+     height: 282,
+     width: double.infinity,
+      decoration: const BoxDecoration(
+        color: AfroReadsColors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(8),
+          topRight: Radius.circular(8),
+        ),
+      ),
+      child: Column(
+        children: [
+           const Gap(20),
+           Container(
+            height: 64,
+            width: 64,
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: AfroReadsColors.primaryColor,
+                )),
+            child: Center(
+              // child: SvgPicture.asset(
+              //   AppAssets.mail2,
+              //   color: AfroReadsColors.primaryColor,
+              //   width: 38,
+              //   height: 30,
+              // ),
+            ),
+          ),
+          const Gap(10),
+          TextSemiBold("Successful !",
+            style: const TextStyle(
+              fontSize: 24
+            ),
+            ),
+            const Gap(10),
+            TextBody("You have successfully",
+            style: const TextStyle(
+              fontSize: 16,
+              color: Colors.black54
+            ),
+            ),
+             TextBody("changed your password",
+            style: const TextStyle(
+              fontSize: 16,
+              color: Colors.black54
+            ),
+            ),
+            const Gap(25),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 50),
+              child: BusyButton(
+                title: "Continue", 
+                onTap: (){
+                  Navigator.of(context).pop();
+                }
+                ),
+            )
+        ],
+      ),
+    );
+  }
+}
