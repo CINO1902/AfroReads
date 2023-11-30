52b95e1e790a2ff3068143425e42a18fff3792ce
ibidapo <ibidapodavid10@gmail.com>
2023-11-30 20:13:15 +0000

    home page ui updated

---
+++ lib/features/getbooks/presentation/pages/book_type/fiction.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Fiction extends StatefulWidget {
+  const Fiction({super.key});
+
+  @override
+  State<Fiction> createState() => _FictionState();
+}
+
+class _FictionState extends State<Fiction> {
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+      color: Colors.black,
+    );
+  }
+}
---
+++ lib/features/getbooks/presentation/pages/book_type/lifestyle.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Lifestyle extends StatefulWidget {
+  const Lifestyle({super.key});
+
+  @override
+  State<Lifestyle> createState() => _LifestyleState();
+}
+
+class _LifestyleState extends State<Lifestyle> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blueGrey,
+    );
+  }
+}
